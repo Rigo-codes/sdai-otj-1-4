b89fbc87a9b308b6b299c43df261d3f39e8d4caf
Rigo-codes <123211975+Rigo-codes@users.noreply.github.com>
2024-09-05 01:58:08 +0000

    added fade in animation to header image and sections.

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const headerImage = document.querySelector('.header-image');
+    
+    // Wait a brief moment, then fade in the image
+    setTimeout(() => {
+        headerImage.style.opacity = 1;
+    }, 500); // 500 ms delay before starting fade-in
+
+    const menu = document.querySelector('.menu');
+    
+    // Wait a brief moment, then fade in the image
+    setTimeout(() => {
+        menu.style.opacity = 1;
+    }, 500); // 500 ms delay before starting fade-in
+});
+
+
